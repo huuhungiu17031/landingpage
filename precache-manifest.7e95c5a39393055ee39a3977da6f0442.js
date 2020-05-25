0d3fe035b1c404b88efe79aed77fb2a0926c5f87
huuhungiu17031 <nguyenhuuhung.9a10.2013@gmail.com>
2020-05-26 01:15:12 +0700

    Updates

---
+++ precache-manifest.7e95c5a39393055ee39a3977da6f0442.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1511df23960221fd33b08cb1c5c32983",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "a5cb207848bc1733e84b",
+    "url": "/landingpage/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dcbcc606f0d81fd0bec6",
+    "url": "/landingpage/static/css/main.172db2d9.chunk.css"
+  },
+  {
+    "revision": "a5cb207848bc1733e84b",
+    "url": "/landingpage/static/js/2.211f8eae.chunk.js"
+  },
+  {
+    "revision": "b93f47e02c0a4d215b626fc9f60ae402",
+    "url": "/landingpage/static/js/2.211f8eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbcc606f0d81fd0bec6",
+    "url": "/landingpage/static/js/main.d5a6a673.chunk.js"
+  },
+  {
+    "revision": "17d663503075dbbffa99",
+    "url": "/landingpage/static/js/runtime-main.21bbea8d.js"
+  },
+  {
+    "revision": "bbf2cda5a7feddbde6b2a8fa612c300f",
+    "url": "/landingpage/static/media/bg2.bbf2cda5.jpg"
+  },
+  {
+    "revision": "8f208990774e70d57b967c5b0a91bec0",
+    "url": "/landingpage/static/media/cv.8f208990.png"
+  },
+  {
+    "revision": "3805016b5f8985d31bc6594480563a11",
+    "url": "/landingpage/static/media/love3000.3805016b.jpg"
+  },
+  {
+    "revision": "5f0115b8f1a40cf066abe42b07f7ea1d",
+    "url": "/landingpage/static/media/p2.5f0115b8.jpeg"
+  },
+  {
+    "revision": "98f4e2351160c6e4baef376d4e5bbaf3",
+    "url": "/landingpage/static/media/p3.98f4e235.jpeg"
+  },
+  {
+    "revision": "9f0f0d04534542b6fbfa58e899cd2c25",
+    "url": "/landingpage/static/media/p4.9f0f0d04.jpeg"
+  },
+  {
+    "revision": "748f654a2ec20a535ddb04ef23bf0420",
+    "url": "/landingpage/static/media/p5.748f654a.jpeg"
+  },
+  {
+    "revision": "1819f19235b2276f9d3cc7742685c88a",
+    "url": "/landingpage/static/media/p6.1819f192.jpeg"
+  },
+  {
+    "revision": "f9d263cfd0b94bcf19a856c1136a6362",
+    "url": "/landingpage/static/media/p7.f9d263cf.jpeg"
+  },
+  {
+    "revision": "63cc566da865ef5ef54da82640d0065b",
+    "url": "/landingpage/static/media/p8.63cc566d.jpeg"
+  },
+  {
+    "revision": "7437041bc0c9c99b5f43b36db230c78a",
+    "url": "/landingpage/static/media/p9.7437041b.png"
+  }
+]);
